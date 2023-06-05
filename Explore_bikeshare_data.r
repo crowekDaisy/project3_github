9151db671d85c268c9fe24d5b5a7d82a96fed9bc
Kristine Crowe <crowek@battelle.org>
2023-06-05 07:32:59 -0400

    Moved files out .github folder

---
+++ Explore_bikeshare_data.r
@@ -0,0 +1,42 @@
+
+ny = read.csv('new_york_city.csv')
+wash = read.csv('washington.csv')
+chi = read.csv('chicago.csv')
+
+head(chi)
+
+library(ggplot2)
+
+ggplot (data=chi,aes(x=Gender))+
+geom_bar(color='red',fill='gray70')+
+geom_text(stat='count', aes(label=..count..),vjust=-1)+
+ggtitle('Counts by Gender in the Chicago Bikeshare')+
+labs(y = "Count",x = "Gender")
+
+
+
+chi = read.csv('chicago.csv')
+library (ggplot2)
+head(chi)
+
+
+ggplot(aes(x=Start.Station),data=subset(chi, End.Station=="Streeter Dr & Grand Ave")) +
+geom_bar(fill='gray70')+
+coord_flip()+
+geom_text(stat='count', aes(label=..count..),hjust=-.1)+
+ggtitle('Starting Stations with Trips to Streeter Dr & Grand Ave')+
+labs(y = "Count",x = "Starting Station")
+
+chi = read.csv('chicago.csv')
+library (ggplot2)
+head(chi)
+
+ggplot (data=chi,aes(x=Trip.Duration/60))+
+geom_histogram(binwidth=1)+
+xlim(0,60)+
+ggtitle('Histogram:Trip Duration for the Chicago Bikeshare')+
+labs(x = "Trip Duration: Minutes",y="Frequency")
+
+
+
+system('python -m nbconvert Explore_bikeshare_data.ipynb')
---
+++ README.md
@@ -0,0 +1,16 @@
+>**Note**: Please **fork** the current Udacity repository so that you will have a **remote** repository in **your** Github account. Clone the remote repository to your local machine. Later, as a part of the project "Post your Work on Github", you will push your proposed changes to the remote repository in your Github account.
+
+### Date Created
+This project was created on 6-3-2023.
+
+### Project Title
+Explore Bikeshare Data in R
+
+### Project Description
+This project explores Bikeshare Data in R using data from Chicago, New York City, and Washington
+
+### Files Used
+Files used: chicago.csv, washington.csv, new_york_city.csv
+
+
+
